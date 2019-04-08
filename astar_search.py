828129fd3373069efdf900367c79db0fa4b33ffc
Zachary Ho <zyyho97@gmail.com>
2019-04-08 22:53:10 +1000

    End of day commit: Defined Gameboard, PriorityQueue classes. I'm in the process of making the a_star_search algorithm, currently figuring out what weights to give for each of the moves.

--- astar_search.py
+++ astar_search.py
@@ -7,37 +7,81 @@ Authors:
 
 import sys
 import json
+import heapq
 
 # Constants
 BOARD_DIM_BOUNDS = 3
+BLOCK_HEXTYPE = 'block'
+COLOUR_PIECE_HEXTYPE = 'colour_piece'
+EXITPOINT_HEXTYPE = 'exitpoint'
 
-# Class for game board
+##################################### CLASS FOR GAME BOARD START #####################################
 class GameBoard:
     def __init__(self):
-        self.colour_pieces = {}
-        self.blocks = {}
+
+        # A dictionary of key:value format -> 'coordinate':'hextype'
+        # hextype can be 'block', 'exitpoint', 'colour_piece'
+        # if coordinate is not in self.hexes, it means that it's empty
+        self.hexes = {}
 
     # Check if coordinate is a block or not
     def isSteppable(self, current_hex):
-      return current_hex not in self.blocks
+        return (not "block" in self.hexes[current_hex])
     
+    # Check if it's possible for colour piece to jump over current coordinate or not
     def isJumpable(self, current_hex, from_hex):
-      (q_current, r_current) = current_hex
-      (q_from, r_from) = from_hex
+        (q_current, r_current) = current_hex
+        (q_from, r_from) = from_hex
+
+        q_move = q_current - q_from
+        r_move = r_current - r_from
+        
+        q_next = q_current + q_move
+        r_next = r_current + r_move
+
+        next_hex = (q_next, r_next)
+
+        # returns a tuple (boolean on whether it is possible to jump or not, hex to land on after jumping)
+        return ((not 'block' in self.hexes[next_hex]) and (not 'colour_piece' in self.hexes[next_hex]) and (self.in_bounds, self, next_hex), next_hex)
 
 
     # Check if coordinate is within the bounds of the game board or not
     def in_bounds(self, current_hex):
-      (q, r) = current_hex
-      return (abs(q)<=3 and abs(r)<=3 and abs(q+r)<=3)
+        (q, r) = current_hex
+        return (abs(q)<=3 and abs(r)<=3 and abs(q+r)<=3)
 
-    def neighbours(self, current_hex):
-      (q, r) = current_hex
-      results = [(q, r-1), (q+1, r-1), (q+1, r), (q, r+1), (q-1, r+1), (q-1, r)]
-      results.filter(self.isSteppable, results)
-      results.filter(self.in_bounds, results)
-      return results
+    # Returns a list of hex coordinates that can be the next move
+    # Includes jump moves
+    def next_possible_moves(self, current_hex):
+        (q, r) = current_hex
+        results = [(q, r-1), (q+1, r-1), (q+1, r), (q, r+1), (q-1, r+1), (q-1, r)]
 
+        for step in results:
+            if not self.isSteppable(self, step):
+                is_jumpable_result = self.isJumpable(self, step, current_hex)
+                if self.is_jumpable_result[0]:
+                    results.append(is_jumpable_result[1])
+                else: 
+                    results.remove(step)
+                    
+        results = filter(self.in_bounds, results)
+        return results
+
+##################################### CLASS FOR PRIORITYQUEUE #####################################
+class PriorityQueue:
+    def __init__(self):
+        self.elements = []
+
+    def empty(self):
+        return len(self.elements) == 0
+
+    def put(self, item, priority):
+        heapq.heappush(self.elements, (priority, item))
+
+    def get(self):
+        return heapq.heappop(self.elements)[1]
+
+##################################### OTHER HELPER FUNCTIONS #####################################
 
 # Convert axial coordinates to cube coordinates
 def axial_to_cube(hex):
@@ -47,7 +91,7 @@ def axial_to_cube(hex):
     return (x, y, z) 
 
 # Returns a list of endpoints in tuple/coordinate format depending on which colour is in play
-def get_goals(colour, blockers):
+def get_goals(colour, board):
     if colour == 'red':
         end_points = [(3, -3), (3, -2), (3, -1), (3, 0)]
     elif colour == 'blue':
@@ -57,10 +101,31 @@ def get_goals(colour, blockers):
 
     exit_points = []
     for location in end_points:
-        if location not in blockers:
+        if (not location in board.hexes.keys()):
             exit_points.append(location)
+        elif board.hexes[location] != BLOCK_HEXTYPE:
+            exit_points.append(location)
+
     return exit_points
 
+def a_star_search(board, start):
+    frontier = PriorityQueue()
+    frontier.put(start, 0)
+    came_from = {}
+    cost_so_far = {}
+    cost_so_far[start] = 0
+
+    while not frontier.empty():
+        current = frontier.get()
+
+        if (current in board.hexes.keys()) and (board.hexes[current] == EXITPOINT_HEXTYPE):
+            break
+    
+        for next_move in board.next_possible_moves:
+            new_cost = cost_so_far[current] + 
+
+
+##################################### MAIN AND PRINT HELPER #####################################
 
 def main():
     with open(sys.argv[1]) as file:
@@ -70,7 +135,6 @@ def main():
     ran = range(-3, +3 + 1)
     board_dict = {}
 
-    cost_so_far = {}
     for q in ran:
         for r in ran:
             if -q-r in ran:
@@ -79,27 +143,28 @@ def main():
     # for qr in [(q, r) for q in ran for r in ran if -q - r in ran]:
     #     cost_so_far[qr] = -1
 
+    # Initiate Game Board
+    game_board = GameBoard()
+
     # import data to three lists
     colour_in_play = data['colour']
 
-    # Make a list of tuples for all the block pieces
-    block_pieces = []
+    # Update game_board hexes with blocks coordinates
     for block in data['blocks']:
         block = tuple(block)
-        block_pieces.append(block)
+        game_board.hexes[block] = BLOCK_HEXTYPE
         board_dict[block] = '###'
 
-    # Make a list of tuples for all the playing pieces
-    play_pieces = []
+    # Update game_board hexes with colour_pieces coordinates
     for piece in data['pieces']:
         piece = tuple(piece)
-        play_pieces.append(piece)
+        game_board.hexes[piece] = COLOUR_PIECE_HEXTYPE
         board_dict[piece] = colour_in_play
+         
+    for goal in get_goals(colour_in_play, game_board):
+        game_board.hexes[goal] = EXITPOINT_HEXTYPE
 
-    goals = get_goals(colour_in_play, block_pieces)
-
-    # Initiate Game Board
-    game_board = GameBoard()
+    
 
 
 
